8c351bf750661807bd916bbefea7a9b1cb213fae
Duc Le <d.le@sap.com>
2024-07-17 18:34:06 +0000

    removed unused imports

--- earnest.py
+++ earnest.py
@@ -1,6 +1,5 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.documents import Document
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
